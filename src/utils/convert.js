5406f5977d436e6ed50f5dae4a169db8137e9f00
MSILycanthropy <53885212+MSILycanthropy@users.noreply.github.com>
2021-04-21 16:45:21 -0500

    Comment cleanup

--- src/utils/convert.js
+++ src/utils/convert.js
@@ -1,3 +1,4 @@
+// Converts audio to the correct format to be processed by picovoice
 function chunkArray(array, size) {
   return Array.from({ length: Math.ceil(array.length / size) }, (v, index) =>
     array.slice(index * size, index * size + size)
